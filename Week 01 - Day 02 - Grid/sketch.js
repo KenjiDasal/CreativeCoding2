943165dfd2b04bcbe32dcee35ad10588ed6fac99
KenjiDasal <dasalkenji@gmail.com>
2022-02-02 12:11:23 +0000

    K: Week 01 - Day 02 - Grid

--- Week 01 - Day 02 - Grid/sketch.js
+++ Week 01 - Day 02 - Grid/sketch.js
@@ -12,28 +12,15 @@ function draw(){
 function drawBox(xOffset, yOffset, boxSize, spacing, colBoxes, rowBoxes, red, green, blue){
     noFill();
     stroke(red, green, blue);
+
     let totalSpace = boxSize + spacing;
-    for(let row = 0; row < rowBoxes; row++){
-        for(let col = 0; col < colBoxes; col++){
-            rect(row * totalSpace + xOffset, col * totalSpace + yOffset, boxSize, boxSize);
+
+    for(let row = 0; row < rowBoxes; row++){ //this creates a loop for each row of boxes.
+        
+        for(let col = 0; col < colBoxes; col++){ //this creates a loop for each column of boxes.
+
+            rect(row * totalSpace + xOffset, col * totalSpace + yOffset, boxSize, boxSize); //this is a rect() that has many parameters that can be manipulated on console of the website.
         }
     }
+    return 'finished making the boxes';
 }
-
-
-
-function text(startNumber, endNumber){
-    let loopNumber = endNumber-startNumber
-    for(let i = 0; i <= loopNumber; i++){
-        console.log("testing");
-    }
-
-    return "done";
-}
-
-function sum(num1, num2){
-    let total = num1 + num2; 
-    console.log("all my work is done")
-    return total;
-}
-
