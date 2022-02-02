86d7246bb032cf777d3b9becbf68971e4f39ea7a
KenjiDasal <dasalkenji@gmail.com>
2022-02-02 14:01:36 +0000

    K: Week 01 - Day 02 - Grid

--- Week 01 - Day 02 - Grid/sketch.js
+++ Week 01 - Day 02 - Grid/sketch.js
@@ -1,26 +1,18 @@
 
 
+    let val = 4;
 function setup(){
     createCanvas(500, 500);
-    background(127);
+    background(127, 127, 127 );
+    angleMode(DEGREES);
+    rectMode(CENTER);
 }
 
 function draw(){
-   drawBox();
-}
+    // translate(width/2, height/2);
+    // push()
 
-function drawBox(xOffset, yOffset, boxSize, spacing, colBoxes, rowBoxes, red, green, blue){
-    noFill();
-    stroke(red, green, blue);
-
-    let totalSpace = boxSize + spacing;
-
-    for(let row = 0; row < rowBoxes; row++){ //this creates a loop for each row of boxes.
-        
-        for(let col = 0; col < colBoxes; col++){ //this creates a loop for each column of boxes.
-
-            rect(row * totalSpace + xOffset, col * totalSpace + yOffset, boxSize, boxSize); //this is a rect() that has many parameters that can be manipulated on console of the website.
-        }
-    }
-    return 'finished making the boxes';
+    // rotate(val++)
+   gridBoxes(2, color(75, 75, 255), 2);
+    // pop()
 }
