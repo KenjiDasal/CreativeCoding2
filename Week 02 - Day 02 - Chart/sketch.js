3df002651a712c5cf2a4421474ffa5fdb960b3fc
KenjiDasal <dasalkenji@gmail.com>
2022-02-09 13:17:15 +0000

    K: Week 02 - Day 02 - Chart

--- Week 02 - Day 02 - Chart/sketch.js
+++ Week 02 - Day 02 - Chart/sketch.js
@@ -1,63 +1,81 @@
 let chartWidth = 400;
 let chartHeight = 400;
-let data = [230, 390, 120, 50, 30, 90, 120, 400];
-let spacing = 20;
-let margin = 60;
+
+let maxValue;
+let tickInc;
+
+let data = [10, 70, 60, 30, 40, 80, 90, 90, 110];
+let scaledData = [];
+
+let spacing = 5;
+let margin = 20;
 let numTicks = 10;
 let lineLength = 10;
 
 let ticks = chartHeight / numTicks;
 
-
-let availableWidth = chartWidth - (margin * 2) - (spacing * (data.length - 1)) ;
+let availableWidth = chartWidth - (margin * 2) - (spacing * (data.length - 1));
 let barWidth = availableWidth / data.length;
 
 console.log(barWidth);
-console.log(ticks)
+console.log(ticks);
 
-    let val = 4;
-function setup(){
+let val = 4;
+
+function setup() {
     createCanvas(500, 500);
-    background(127, 127, 127 );
+    background(127, 127, 127);
     // angleMode(DEGREES);
     // rectMode(CENTER);
+
+
+    maxValue = max(...data);
+    console.log(maxValue);
+
+    tickInc = Math.round(maxValue / numTicks);
+    console.log(tickInc);
+
+    for (let i = 0; i < data.length; i++) {
+        let tempVal = map(data[i], 0, maxValue, 0, chartHeight)
+        scaledData.push(tempVal);
+    }
 }
 
-function draw(){
+function draw() {
     background(127)
-    
+
     stroke(0)
     strokeWeight(2)
 
     textSize(8);
     textAlign(RIGHT, CENTER);
 
-    translate(50,450);
-    line(0, 0, 0, -400);
-    line(0, 0, 400, 0)
+    translate(50, 450);
+    line(0, 0, 0, -chartHeight);
+    line(0, 0, chartWidth, 0)
 
     fill(255, 255, 0)
 
-    for(let j = 0; j <=  numTicks;  j++){ 
+    for (let j = 0; j <= numTicks; j++) {
         stroke(0)
-        line(0,  ticks * - j,  -lineLength,  ticks * -j);
+        line(0, ticks * -j, -lineLength, ticks * -j);
 
         noStroke();
-        text(ticks * j, -20, ticks * -j);
+        text(round(tickInc * j), -20, ticks * -j);
     }
-    
+
     translate(margin, 0)
-    for (let i = 0; i < data.length; i++){
+    for (let i = 0; i < data.length; i++) {
         noStroke();
         fill('white')
         totalSpacing = barWidth + spacing;
-        rect(totalSpacing * i, 0, barWidth, -data[i])
+        rect(totalSpacing * i, 0, barWidth, -scaledData[i])
 
-        let dataX = barWidth / 2;
-        textAlign(CENTER, CENTER)
-        fill('black');
-        text(data[i], dataX + (totalSpacing  * i), -data[i] - spacing);
+        dataX = barWidth / 2;
+        textAlign(CENTER)
+        fill('cyan');
+        text(round(data[i]), dataX + (totalSpacing * i), -scaledData[i] - spacing);
     }
 
-    
+
 }
