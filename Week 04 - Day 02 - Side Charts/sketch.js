e0c42f363ce79342a48ec42074ae075e16cd2b17
KenjiDasal <dasalkenji@gmail.com>
2022-02-20 21:39:50 +0000

    Week 04 - Day 02 - Horizontal Bar Chart

---
+++ Week 04 - Day 02 - Side Charts/sketch.js
@@ -0,0 +1,32 @@
+let data = [
+    { name: "Oranges", total: "30" },
+    { name: "Bananas", total: "100" },
+    { name: "Lemons", total: "90" },
+    { name: "Limes", total: "20" },
+    { name: "Apples", total: "100" },
+    { name: "Grapes", total: "94" }
+];
+
+
+
+function setup() {
+    createCanvas(800, 800);
+    background(127);
+
+    firstChart = new HorizontalBarChart(data);
+    firstChart.chartWidth = 400;
+    firstChart.chartHeight = 400;
+    firstChart.posX = 100;
+    firstChart.posY = 700;
+    firstChart.updateValue();
+
+
+}
+
+
+
+function draw() {
+    background(0);
+
+    firstChart.render();
+}
